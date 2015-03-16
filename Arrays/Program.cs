71c5377d53f4b9b4f7aa475033aa847ba8845cdd
Chohgin <nickwalker867@gmail.com>
2015-03-16 19:26:56 -0400

    Set Random rolls, made and added to array, created table.

---
+++ Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rolls={};
+            Random dice = new Random();
+
+            for (int i = 0; i < 36000; i++)
+            {
+                int rollOne = dice.Next(1, 6);
+                int rollTwo = dice.Next(1, 6);
+
+                rolls[i] = rollOne + rollTwo;
+            }
+
+            Console.WriteLine("02: {0}");
+            Console.WriteLine("03: {0}");
+            Console.WriteLine("04: {0}");
+            Console.WriteLine("05: {0}");
+            Console.WriteLine("06: {0}");
+            Console.WriteLine("07: {0}");
+            Console.WriteLine("08: {0}");
+            Console.WriteLine("09: {0}");
+            Console.WriteLine("10: {0}");
+            Console.WriteLine("11: {0}");
+            Console.WriteLine("12: {0}");
+
+            
+            
+
+
+        }
+    }
+}
